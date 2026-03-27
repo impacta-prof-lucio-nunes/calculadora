e033a9b3fd56139d935999e6538d75a3a1933a43
Lucio Nunes <lucio.nunes@impacta.edu.br>
2026-03-27 11:38:26 -0300

    Função de multiplicação foi incluída

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -4,10 +4,14 @@ def soma(a, b):
 def subtrai(a, b):
     return a - b
 
+def multiplica(a, b):
+    return a * b
+
 def main():
     x = int(input('x? '))
     y = int(input('y? '))
     print(f'Soma: {soma(x, y)}')
     print(f'Subtração: {subtrai(x, y)}')
+    print(f'Multiplicação: {multiplica(x, y)}')
 
 main()
