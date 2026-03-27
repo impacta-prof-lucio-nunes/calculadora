c7c45df5e32d5fa2ef26c22047b9a85a3601579b
Lucio Nunes <lucio.nunes@impacta.edu.br>
2026-03-27 11:50:00 -0300

    Inclusão da função de divisão

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -7,11 +7,15 @@ def subtrai(a, b):
 def multiplica(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b
+
 def main():
     x = int(input('x? '))
     y = int(input('y? '))
     print(f'Soma: {soma(x, y)}')
     print(f'Subtração: {subtrai(x, y)}')
     print(f'Multiplicação: {multiplica(x, y)}')
+    print(f'Divisão: {divide(x, y)}')
 
 main()
